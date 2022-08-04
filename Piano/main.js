f1ae4ad33a0f58f7807c1b0ae016e696d2419258
Parvez Ahmed <126ahmedparvez@gmail.com>
2022-08-04 11:05:33 +0530

    first

---
+++ Piano/main.js
@@ -0,0 +1,99 @@
+function keyPress(keyPressed){
+    if(keyPressed == "c#1"){
+        var audio = new Audio("sounds/Db3.mp3");
+        audio.play();
+    }
+    if(keyPressed=="c1"){
+        var audio = new Audio("sounds/C3.mp3");
+        audio.play();
+    }
+    if(keyPressed=="d#1"){
+        var audio = new Audio("sounds/Eb3.mp3");
+        
+        audio.play();
+    }
+    if(keyPressed=="d1"){
+        var audio = new Audio("sounds/D3.mp3");
+        audio.play();
+    }
+    if(keyPressed=="e1"){
+        var audio = new Audio("sounds/E3.mp3");
+        audio.play();
+    }
+    if(keyPressed=="f1"){
+        var audio = new Audio("sounds/F3.mp3");
+        audio.play();
+    }
+    if(keyPressed=="f#1"){
+        var audio = new Audio("sounds/Gb3.mp3");
+        audio.play();
+    }
+    if(keyPressed=="g1"){
+        var audio = new Audio("sounds/G3.mp3");
+        audio.play();
+    }
+    if(keyPressed=="g#1"){
+        var audio = new Audio("sounds/Ab3.mp3");
+        audio.play();
+    }
+    if(keyPressed=="a1"){
+        var audio = new Audio("sounds/A3.mp3");
+        audio.play();
+    }
+    if(keyPressed=="a#1"){
+        var audio = new Audio("sounds/Bb3.mp3");
+        audio.play();
+    }
+    if(keyPressed=="b1"){
+        var audio = new Audio("sounds/B3.mp3");
+        audio.play();
+    }
+    if(keyPressed=="c2"){
+        var audio = new Audio("sounds/C4.mp3");
+        audio.play();
+    }
+    if(keyPressed=="c#2"){
+        var audio = new Audio("sounds/Db4.mp3");
+        audio.play();
+    }
+    if(keyPressed=="d2"){
+        var audio = new Audio("sounds/D4.mp3");
+        audio.play();
+    }
+    if(keyPressed=="d#2"){
+        var audio = new Audio("sounds/Eb4.mp3");
+        audio.play();
+    }
+    if(keyPressed=="e2"){
+        var audio = new Audio("sounds/E4.mp3");
+        audio.play();
+    }
+    if(keyPressed=="f2"){
+        var audio = new Audio("sounds/F4.mp3");
+        audio.play();
+    }
+    if(keyPressed=="f#2"){
+        var audio = new Audio("sounds/Gb4.mp3");
+        audio.play();
+    }
+    if(keyPressed=="g2"){
+        var audio = new Audio("sounds/G4.mp3");
+        audio.play();
+    }
+    if(keyPressed=="g#2"){
+        var audio = new Audio("sounds/Ab4.mp3");
+        audio.play();
+    }
+    if(keyPressed=="a2"){
+        var audio = new Audio("sounds/A4.mp3");
+        audio.play();
+    }
+    if(keyPressed=="a#2"){
+        var audio = new Audio("sounds/Bb4.mp3");
+        audio.play();
+    }
+    if(keyPressed=="b2"){
+        var audio = new Audio("sounds/B4.mp3");
+        audio.play();
+    }
+}
